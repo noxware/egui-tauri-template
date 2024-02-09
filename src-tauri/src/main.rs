0814cade4ec81adcd3b248f1ba5459e2a0578b40
noxware <7684329+noxware@users.noreply.github.com>
2024-02-08 23:28:14 -0300

    added invoke without args example

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,13 +2,25 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn secret() -> String {
+    "You are amazing!".into()
+}
+
+#[tauri::command]
+fn greet(name: &str) -> Result<String, String> {
+    if name.is_empty() {
+        Err("You sent an empty name to greet. Now try sending something.".to_owned())
+    } else {
+        Ok(format!(
+            "Hello, {}! You've been greeted from Rust! Now try to send an empty name.",
+            name
+        ))
+    }
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, secret])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,14 @@
 use std::{cell::RefCell, rc::Rc};
 
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 use serde_wasm_bindgen::to_value;
 
 use crate::tauri::invoke;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize)]
+struct EmptyArgs {}
+
+#[derive(Serialize)]
 struct GreetArgs {
     name: String,
 }
@@ -13,6 +16,8 @@ struct GreetArgs {
 pub struct TemplateApp {
     greet_name: String,
     greet_output: Rc<RefCell<String>>,
+    secret_output: Rc<RefCell<String>>,
+    fetch_output: Rc<RefCell<String>>,
 }
 
 impl Default for TemplateApp {
@@ -20,6 +25,8 @@ impl Default for TemplateApp {
         Self {
             greet_name: "Kagome Higurashi".to_owned(),
             greet_output: Rc::new(RefCell::new("".to_owned())),
+            secret_output: Rc::new(RefCell::new("".to_owned())),
+            fetch_output: Rc::new(RefCell::new("".to_owned())),
         }
     }
 }
@@ -33,19 +40,39 @@ impl TemplateApp {
 impl eframe::App for TemplateApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Examples");
+            ui.heading("Invoke without args");
 
+            if self.secret_output.borrow().is_empty() {
+                if ui.button("Reveal the secret").clicked() {
+                    let output = self.secret_output.clone();
+                    wasm_bindgen_futures::spawn_local(async move {
+                        let response = invoke("secret", to_value(&EmptyArgs {}).unwrap())
+                            .await
+                            .as_string()
+                            .unwrap();
+                        *output.borrow_mut() = response;
+                    });
+                }
+            } else {
+                ui.label(&*self.secret_output.borrow());
+            }
+
+            ui.add_space(16.0);
+            ui.separator();
+            ui.add_space(16.0);
+
+            ui.heading("Invoke with args");
             ui.horizontal(|ui| {
                 ui.label("Write something: ");
                 ui.text_edit_singleline(&mut self.greet_name);
-                if ui.button("Send").clicked() {
+                if ui.button("Greet").clicked() {
                     let args = to_value(&GreetArgs {
                         name: self.greet_name.clone(),
                     })
                     .unwrap();
                     let output = self.greet_output.clone();
                     wasm_bindgen_futures::spawn_local(async move {
-                        let response: String = invoke("greet", args).await.as_string().unwrap();
+                        let response = invoke("greet", args).await.as_string().unwrap();
                         *output.borrow_mut() = response;
                     });
                 }
@@ -57,7 +84,9 @@ impl eframe::App for TemplateApp {
                 ui.label(format!("Response: {}", self.greet_output.borrow()));
             }
 
+            ui.add_space(16.0);
             ui.separator();
+            ui.add_space(16.0);
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
                 ui.horizontal(|ui| {
